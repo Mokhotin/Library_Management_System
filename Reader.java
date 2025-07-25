06c122b8eab4a5b7b79698ce44170eea60aebc54
Smithmok <mokhotin1@gmail.com>
2025-07-25 22:18:21 +0800

    Create Reader.java

---
+++ Reader.java
@@ -0,0 +1,58 @@
+package library;
+
+/**
+ * The Reader class represents a reader in a library.
+ * It contains information about the reader, such as their ID, name, and contact details.
+ * It provides a method to display the reader information.
+ * 
+ * @author MOK Ho Tin (23637498)
+ * @version FINT2100 Project
+ * @since 2024-04-24
+ */
+public class Reader {
+    
+    // Reader Attributes
+    /**
+     * The unique identifier of the reader.
+     */
+    private String id;
+    /**
+     * The name of the reader.
+     */
+    private String name;
+    /**
+     * The email contact of the reader.
+     */
+    private String contact;
+
+    /**
+     * Constructs a Reader object with the specified information.
+     *
+     * @param id      the ID of the reader
+     * @param name    the name of the reader
+     * @param contact the email contact of the reader
+     */
+    public Reader(String id, String name, String contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    /**
+     * Displays the information of the reader.
+     */
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Contact: " + contact);
+    }
+
+    /**
+     * Returns the ID of the reader.
+     *
+     * @return the ID of the reader
+     */
+    public String getId() {
+        return id;
+    }
+}
